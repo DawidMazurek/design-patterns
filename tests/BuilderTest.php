962ad6af9e0f57da37654f79aa13f04d4b5b98d8
Dawid <dawid@dawidmazurek.pl>
2014-06-04 21:01:33 +0200

    Test test

--- tests/BuilderTest.php
+++ tests/BuilderTest.php
@@ -1,11 +1,10 @@
 <?php
-require_once "Builder.php";
+require_once "../Builder/Builder.php";
 
 class BuilderTest extends \PHPUnit_Framework_TestCase {
 
 
     public function testBuilder() {
-        echo "a";
         $builder = new Builder();
         $this->assertInstanceOf( 'Builder', $builder, "Instance of builder" );
     }
