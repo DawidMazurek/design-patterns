8d3c6cc2af1d652ad2ad055def83ead5423d0130
Dawid <dawid@dawidmazurek.pl>
2014-06-04 22:24:39 +0200

    phpunit test

--- tests/BuilderTest.php
+++ tests/BuilderTest.php
@@ -1,13 +1,13 @@
 <?php
-//require_once "../Builder/Builder.php";
+require_once "../Builder/Builder.php";
 
 class BuilderTest extends \PHPUnit_Framework_TestCase {
 
 
     public function testBuilder() {
-        //$builder = new Builder();
-        //$this->assertInstanceOf( 'Builder', $builder, "Instance of builder" );
-        $this->assertTrue( true );
+        $builder = new Builder();
+        $this->assertInstanceOf( 'Builder', $builder, "Instance of builder" );
+        //$this->assertTrue( true );
     }
 
 
