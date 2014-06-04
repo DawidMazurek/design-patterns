d442ae8c52e47023786688b98baca399f875da61
Dawid <dawid@dawidmazurek.pl>
2014-06-04 22:39:36 +0200

    phpunit test

--- tests/BuilderTest.php
+++ tests/BuilderTest.php
@@ -6,7 +6,7 @@ class BuilderTest extends \PHPUnit_Framework_TestCase {
 
     public function testBuilder() {
         $builder = new Builder\Builder();
-        $this->assertInstanceOf( 'Builder', $builder, "Instance of builder" );
+        $this->assertInstanceOf( 'Builder\Builder', $builder, "Instance of builder" );
 
     }
 
